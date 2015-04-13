8867a7cd3835970e23c56c43e55cd1a30a8a862c
himanshusaurabh <himanshum2795@gmail.com>
2015-04-14 01:34:26 +0530

    Create js2.js

---
+++ js2.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<script type="text/javascript">
+<-- 
+function ramu()
+{alert("Ramu is back")}
+//-->
+</script>
+</head>
+<body>
+<input type="button" onclick="ramu()" value="Say Hellow Ramu"/>
+</body>
+</html>
